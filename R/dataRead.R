1f8ea9aac6b7a4635eeb1ddf61df0071ad3144d2
calebgreski <calebdotexe@gmail.com>
2022-07-17 19:58:45 -0400

    Altered how variables are read in. Final story tests are passed.
    Wide is now converted to long before data validation.

--- R/dataRead.R
+++ R/dataRead.R
@@ -9,26 +9,33 @@ dataRead <- function(dataFrame, timing, streams, VoI = NA, type="long", median0 
 
   if(type=="wide"){
     message("Data is wide")
-    for(i in 1:length(streams)){
-      if(length(streams[[i]])!=length(streams[[1]])){
-        stop("Input dataframe needs equal sample count")
-      }
-    }
+    #for(i in 1:length(streams)){
+    #  if(length(streams[[i]])!=length(streams[[1]])){
+    #    stop("Input dataframe needs equal sample count")
+    #  }
+    #}
+    dataFrame <- pivot_longer(dataFrame, cols = streams, names_to = "streams", values_to = "VoI")
     ## Do conversion
-    x <- dataFrame
-    return(x)
-  }else
+    colnames(dataFrame) <- c("timing", "streams", "VoI")
+  }
 
-  ifelse(streams == "", stop("Input stream variable has missing values"), NA)
-  ifelse(is.numeric(timing), NA, stop("Input time variable is non-numeric"))
-  ifelse(is.na(timing), stop("Input time variable has missing values"), NA)
-  ifelse(is.numeric(VoI), NA, stop("Input variable of interest is non-numeric"))
-  ifelse(is.na(VoI), stop("Input variable of interest has missing values"), NA)
+  if(type=="long"){
+  names(dataFrame)[names(dataFrame) == timing] <- "timing"
+  names(dataFrame)[names(dataFrame) == streams] <- "streams"
+  names(dataFrame)[names(dataFrame) == VoI] <- "VoI"
+  }
 
-  if(length(dataFrame[[1]])%%length(unique(timing))!=0){
+
+  ifelse(dataFrame$streams == "", stop("Input stream variable has missing values"), NA)
+  ifelse(is.numeric(dataFrame$timing), NA, stop("Input time variable is non-numeric"))
+  ifelse(is.na(dataFrame$timing), stop("Input time variable has missing values"), NA)
+  ifelse(is.numeric(dataFrame$VoI), NA, stop("Input variable of interest is non-numeric"))
+  ifelse(is.na(dataFrame$VoI), stop("Input variable of interest has missing values"), NA)
+
+  if(length(dataFrame[[1]])%%length(unique(dataFrame$timing))!=0){
     stop("Input dataframe needs equal sample count")
   }else
-  if(length(dataFrame[[1]])%%length(unique(streams))!=0){
+  if(length(dataFrame[[1]])%%length(unique(dataFrame$streams))!=0){
     stop("Input dataframe needs equal sample count")
   }
   if(is.na(median0) == TRUE){
@@ -53,3 +60,4 @@ dataRead <- function(dataFrame, timing, streams, VoI = NA, type="long", median0 
 
 }
 
+dataRead(x, timing="hour", streams=c("rep1", "rep2", "rep3", "rep4", "rep5", "rep6", "rep7", "rep8", "rep9", "rep10"), type="wide", median0 = .8)
--- R/dataReadTests.R
+++ R/dataReadTests.R
@@ -11,6 +11,7 @@ if(!require(tidyr)){
 }
 
 x <- read.csv("./testData.csv")
+xshort <- read.csv("./testDataShort.csv")
 xl <- pivot_longer(x, cols = c("rep1", "rep2", "rep3", "rep4", "rep5", "rep6", "rep7", "rep8", "rep9", "rep10"), names_to = "rep", values_to = "time")
 xlshort <- xl[c(-145),]
 write.csv(xl, "./longTestData.csv")
@@ -19,6 +20,8 @@ xlerrorTNA <- read.csv("./longTestDataErrorMissingTime.csv")
 xlerrorTCH <- read.csv("./longTestDataErrorCharTime.csv")
 xlerrorHNA <- read.csv("./longTestDataErrorMissingHour.csv")
 xlerrorHCH <- read.csv("./longTestDataErrorCharHour.csv")
+expOutput <- xl
+colnames(expOutput) <- c("timing", "streams", "VoI")
 
 
 ## function(dataFrame, timing, streams, VoI = NA, type="long", median0 = NA, delta = 3) ##
@@ -26,23 +29,23 @@ xlerrorHCH <- read.csv("./longTestDataErrorCharHour.csv")
 
 test_that("dataRead program works", {
     source("./R/dataRead.R")
-    expect_message(dataRead(x, timing=x$hour, streams=c(x$rep1,x$rep2,x$rep3), type="wide"), "Data is wide")      # Check for wide data
-    expect_error(dataRead(x, streams=c(x$rep1,x$rep2,x$rep3), type="wide"), "Input dataframe needs timing variable")            # Check missing time variable
-    expect_error(dataRead(x, x$hour, type="wide"), "Input dataframe needs stream identifier variable")          # Check for stream identifier
-    expect_error(dataRead(xlshort, xlshort$time, xlshort$hour, streams=xlshort$rep, type="long"), "Input dataframe needs equal sample count")       # Check for equal sample count in long data
-    expect_error(dataRead(x, x$hour, streams=(list(x$rep1, x$rep10[-1])), type="wide"), "Input dataframe needs equal sample count")             # Check for equal sample count in wide data
+    expect_message(dataRead(x, timing="hour", streams=c("rep1", "rep2", "rep3", "rep4", "rep5", "rep6", "rep7", "rep8", "rep9", "rep10"), type="wide", median0 = .8), "Data is wide")      # Check for wide data
+    expect_error(dataRead(x, streams=c("rep1", "rep2", "rep3", "rep4", "rep5", "rep6", "rep7", "rep8", "rep9", "rep10"), type="wide"), "Input dataframe needs timing variable")            # Check missing time variable
+    expect_error(dataRead(x, "hour", type="wide"), "Input dataframe needs stream identifier variable")          # Check for stream identifier
+    expect_error(dataRead(xlshort, "hour", "time", streams="rep", type="long"), "Input dataframe needs equal sample count")       # Check for equal sample count in long data
+    expect_error(dataRead(xshort, timing="hour", streams=c("rep1", "rep2", "rep3", "rep4", "rep5", "rep6", "rep7", "rep8", "rep9", "rep10"), type="wide", median0 = .8), "Input dataframe needs equal sample count")             # Check for equal sample count in wide data
     #expect_error(dataRead(xlerror1, xlerror1$hour, streams=xlerror1$rep, xlerror1$time, type="long"), "Input variable of interest is non-numeric or has missing values")  #Missing value test
-    expect_error(dataRead(xlerrorSNA, xlerrorSNA$hour, streams=xlerrorSNA$rep, xlerrorSNA$time, type="long"), "Input stream variable has missing values")  #Char value test
-    expect_error(dataRead(xlerrorHNA, xlerrorHNA$hour, streams=xlerrorHNA$rep, xlerrorHNA$time, type="long"), "Input time variable has missing values")  #Missing value test ########
-    expect_error(dataRead(xlerrorHCH, xlerrorHCH$hour, streams=xlerrorHCH$rep, xlerrorHCH$time, type="long"), "Input time variable is non-numeric")  #Char value test ########
-    expect_error(dataRead(xlerrorTNA, xlerrorTNA$hour, streams=xlerrorTNA$rep, xlerrorTNA$time, type="long"), "Input variable of interest has missing values")  #Missing value test Target
-    expect_error(dataRead(xlerrorTCH, xlerrorTCH$hour, streams=xlerrorTCH$rep, xlerrorTCH$time, type="long"), "Input variable of interest is non-numeric")  #Char value test Target
-    expect_error(dataRead(xl, xl$time, xl$hour, streams=xl$rep, type="long"), "Target median is missing")             # Check for target median
-    expect_error(dataRead(xl, xl$time, xl$hour, streams=xl$rep, type="long", median0 = "hello"), "Inputted target median is non-numeric")             # Check for target median numeric
-    expect_error(dataRead(xl, xl$time, xl$hour, streams=xl$rep, type="long", median0 = 3), "Please input a target median less than 1")             # Check for target median less than 1
-    expect_error(dataRead(xl, xl$time, xl$hour, streams=xl$rep, type="long", median0 = -1), "Please input a target median greater than 0")             # Check for target median greater than 0
-    expect_error(dataRead(xl, xl$time, xl$hour, streams=xl$rep, type="long", median0 = .8, delta = "hello"), "Inputted target delta is non-numeric")             # Check for delta numeric
-    expect_equal(x, dataRead(x, x$hour, streams=c(x$rep1,x$rep2,x$rep3), type="wide"))             # Check for wide output dataframe
-    expect_equal(xl, dataRead(xl, xl$time, xl$hour, streams=xl$rep, type="long", median0 = .8))             # Check for long output dataframe
+    expect_error(dataRead(xlerrorSNA, "hour", streams="rep", "time", type="long"), "Input stream variable has missing values")  #Char value test
+    expect_error(dataRead(xlerrorHNA, "hour", "time", streams="rep", type="long"), "Input time variable has missing values")  #Missing value test ########
+    expect_error(dataRead(xlerrorHCH, "hour", "time", streams="rep", type="long"), "Input time variable is non-numeric")  #Char value test ########
+    expect_error(dataRead(xlerrorTNA, "hour", "time", streams="rep", type="long"), "Input variable of interest has missing values")  #Missing value test Target
+    expect_error(dataRead(xlerrorTCH, "hour", "time", streams="rep", type="long"), "Input variable of interest is non-numeric")  #Char value test Target
+    expect_error(dataRead(xl, "hour", "time", streams="rep", type="long"), "Target median is missing")             # Check for target median
+    expect_error(dataRead(xl, "hour", "time", streams="rep", type="long", median0 = "hello"), "Inputted target median is non-numeric")             # Check for target median numeric
+    expect_error(dataRead(xl, "hour", "time", streams="rep", type="long", median0 = 3), "Please input a target median less than 1")             # Check for target median less than 1
+    expect_error(dataRead(xl, "hour", "time", streams="rep", type="long", median0 = -1), "Please input a target median greater than 0")             # Check for target median greater than 0
+    expect_error(dataRead(xl, "hour", "time", streams="rep", type="long", median0 = .8, delta = "hello"), "Inputted target delta is non-numeric")             # Check for delta numeric
+    expect_equal(expOutput, dataRead(x, "hour", streams=c("rep1", "rep2", "rep3", "rep4", "rep5", "rep6", "rep7", "rep8", "rep9", "rep10"), type="wide", median0 = .8))             # Check for wide output dataframe
+    expect_equal(expOutput, dataRead(xl, "hour", "time", streams="rep", type="long", median0 = .8))             # Check for long output dataframe
 })
 dataRead(xlshort, xlshort$hour, streams=xlshort$rep, type="long")
